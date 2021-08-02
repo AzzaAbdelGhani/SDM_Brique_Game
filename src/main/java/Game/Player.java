6ae222234f730f7d28eb3b1f40362a2b4b464f14
AzzaAbdelGhani <azza.mazher90@gmail.com>
2021-08-02 19:26:40 +0200

    initialize for each player his own graph with respect to his pieceColor

--- src/main/java/Game/Player.java
+++ src/main/java/Game/Player.java
@@ -4,20 +4,24 @@ public class Player {
     private String name;
     private Piece_Color pieceColor; //each player choose a color to play with
     private Boolean active = Boolean.FALSE;
+    private Graph graph;
 
     public Player(String name, Piece_Color pieceColor)
     {
         this.name = name;
         this.pieceColor = pieceColor;
+        this.graph = new Graph(this.pieceColor);
     }
 
     // getters
-    public String getName() {return name;}
-    public Piece_Color getColor() {return pieceColor;}
-    public Boolean IsActive() {return active;}
+    public String getName() {return this.name;}
+    public Piece_Color getColor() {return this.pieceColor;}
+    public Boolean IsActive() {return this.active;}
+    public Graph getGraph() {return this.graph;}
 
     //setters
     public void setName(String name){this.name = name;}
     public void setColor(Piece_Color pieceColor){this.pieceColor = pieceColor;}
     public void setActive(Boolean active) {this.active = active;}
+    public void setGraph(Graph graph) {this.graph = graph;}
 }
