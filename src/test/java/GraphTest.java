6dd3053e529c3ed139ea36a6f27caeede46206ab
AzzaAbdelGhani <azza.mazher90@gmail.com>
2021-08-05 11:11:59 +0200

    Test areBoredersConnected method

--- src/test/java/GraphTest.java
+++ src/test/java/GraphTest.java
@@ -24,4 +24,26 @@ public class GraphTest {
         g2.add_node(1,5);
         assertTrue(g2.Adjacency_List.get(5).contains(20));
     }
+
+    @Test
+    public void checkConnectionBetweenBorders()
+    {
+        Graph g3 = new Graph(Piece_Color.BLACK);
+        g3.add_node(10, 7);
+        g3.add_node(8, 7);
+        g3.add_node(0, 7);
+        g3.add_node(6, 7);
+        g3.add_node(14, 7);
+        g3.add_node(1, 7);
+        g3.add_node(5, 7);
+        g3.add_node(2, 7);
+        g3.add_node(7, 7);
+        g3.add_node(3, 7);
+        g3.add_node(12, 7);
+        g3.add_node(4, 7);
+        g3.add_node(13, 7);
+        g3.add_node(9, 7);
+        g3.add_node(11, 7);
+        assertTrue(g3.areBordersConnected());
+    }
 }
