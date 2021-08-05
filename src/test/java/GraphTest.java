e744018847f78911ca7074bb7c1f2351e1e8cabf
AzzaAbdelGhani <azza.mazher90@gmail.com>
2021-08-05 11:13:52 +0200

    Refactor GraphTest class

--- src/test/java/GraphTest.java
+++ src/test/java/GraphTest.java
@@ -19,31 +19,29 @@ public class GraphTest {
     @Test
     public void checkEdgesBetweenNodes()
     {
-        Graph g2 = new Graph(Piece_Color.BLACK);
-        g2.add_node(0,5);
-        g2.add_node(1,5);
-        assertTrue(g2.Adjacency_List.get(5).contains(20));
+        g.add_node(0,5);
+        g.add_node(1,5);
+        assertTrue(g.Adjacency_List.get(5).contains(20));
     }
 
     @Test
     public void checkConnectionBetweenBorders()
     {
-        Graph g3 = new Graph(Piece_Color.BLACK);
-        g3.add_node(10, 7);
-        g3.add_node(8, 7);
-        g3.add_node(0, 7);
-        g3.add_node(6, 7);
-        g3.add_node(14, 7);
-        g3.add_node(1, 7);
-        g3.add_node(5, 7);
-        g3.add_node(2, 7);
-        g3.add_node(7, 7);
-        g3.add_node(3, 7);
-        g3.add_node(12, 7);
-        g3.add_node(4, 7);
-        g3.add_node(13, 7);
-        g3.add_node(9, 7);
-        g3.add_node(11, 7);
-        assertTrue(g3.areBordersConnected());
+        g.add_node(10, 7);
+        g.add_node(8, 7);
+        g.add_node(0, 7);
+        g.add_node(6, 7);
+        g.add_node(14, 7);
+        g.add_node(1, 7);
+        g.add_node(5, 7);
+        g.add_node(2, 7);
+        g.add_node(7, 7);
+        g.add_node(3, 7);
+        g.add_node(12, 7);
+        g.add_node(4, 7);
+        g.add_node(13, 7);
+        g.add_node(9, 7);
+        g.add_node(11, 7);
+        assertTrue(g.areBordersConnected());
     }
 }
