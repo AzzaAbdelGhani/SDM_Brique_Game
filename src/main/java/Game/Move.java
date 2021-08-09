cc8f9305964efb1f29eec5320291426025a99cb3
KEERTHANA CHANDRASEKAR <cj.keerthana96@gmail.com>
2021-08-09 13:07:49 +0200

    Refactor Move class - Update the graph of each player while filling the position in the board.

--- src/main/java/Game/Move.java
+++ src/main/java/Game/Move.java
@@ -19,19 +19,25 @@ public class Move {
     public int getX() { return this.x; }
     public int getY() { return this.y; }
 
+    public void fillBoardAndUpdateGraph(int i, int j, Piece_Color color)
+    {
+        board.setPiece(i, j, color);
+        currentPlayer.updateGraph(board,i,j);
+    }
+
     public void fillEscorts(){
         Piece_Color current = currentPlayer.getColor();
         Pos_Color color = board.getPosColor(x, y);
         int size = board.getSize();
         if ( x != size-1 && y != size-1 ) {
-            if (current == board.getPosFill(x+1, y+1) && color == Pos_Color.LIGHT) { board.setPiece(x, y+1, current); }
-            if (current == board.getPosFill(x+1, y+1) && color == Pos_Color.DARK) { board.setPiece(x +1, y, current); }
+            if (current == board.getPosFill(x+1, y+1) && color == Pos_Color.LIGHT) { fillBoardAndUpdateGraph(x, y+1, current); }
+            if (current == board.getPosFill(x+1, y+1) && color == Pos_Color.DARK) { fillBoardAndUpdateGraph(x +1, y, current); }
         }
         if ( x != 0 && y != 0 ){
-            if (current == board.getPosFill(x-1, y-1) && color == Pos_Color.LIGHT) { board.setPiece(x -1, y, current); }
-            if (current == board.getPosFill(x-1, y-1) && color == Pos_Color.DARK){ board.setPiece(x, y -1, current);}
+            if (current == board.getPosFill(x-1, y-1) && color == Pos_Color.LIGHT) { fillBoardAndUpdateGraph(x -1, y, current); }
+            if (current == board.getPosFill(x-1, y-1) && color == Pos_Color.DARK){ fillBoardAndUpdateGraph(x, y -1, current);}
         }
-        if (x == size-1 && color == Pos_Color.DARK) { board.setPiece(x, y +1, current); }
+        if (x == size-1 && color == Pos_Color.DARK) { fillBoardAndUpdateGraph(x, y +1, current); }
         
     }
 
