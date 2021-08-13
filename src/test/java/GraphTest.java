7eaf1f08db60ed5424a6942903e1c6cbe493959a
KEERTHANA CHANDRASEKAR <cj.keerthana96@gmail.com>
2021-08-13 15:45:51 +0200

    Refactor Graph Class - Tests changed for the methods with change in signature

--- src/test/java/GraphTest.java
+++ src/test/java/GraphTest.java
@@ -12,36 +12,36 @@ public class GraphTest {
     @Test
     public void isAddingNodeProperly()
     {
-        g.add_node(0,7);
+        g.add_node(new Coordinates(0,7));
         assertEquals(7, g.Adjacency_List.get(7).get(0));
     }
 
     @Test
     public void checkEdgesBetweenNodes()
     {
-        g.add_node(0,5);
-        g.add_node(1,5);
+        g.add_node(new Coordinates(0,5));
+        g.add_node(new Coordinates(1,5));
         assertTrue(g.Adjacency_List.get(5).contains(20));
     }
 
     @Test
     public void checkConnectionBetweenBorders()
     {
-        g.add_node(10, 7);
-        g.add_node(8, 7);
-        g.add_node(0, 7);
-        g.add_node(6, 7);
-        g.add_node(14, 7);
-        g.add_node(1, 7);
-        g.add_node(5, 7);
-        g.add_node(2, 7);
-        g.add_node(7, 7);
-        g.add_node(3, 7);
-        g.add_node(12, 7);
-        g.add_node(4, 7);
-        g.add_node(13, 7);
-        g.add_node(9, 7);
-        g.add_node(11, 7);
+        g.add_node(new Coordinates(10, 7));
+        g.add_node(new Coordinates(8, 7));
+        g.add_node(new Coordinates(0, 7));
+        g.add_node(new Coordinates(6, 7));
+        g.add_node(new Coordinates(14, 7));
+        g.add_node(new Coordinates(1, 7));
+        g.add_node(new Coordinates(5, 7));
+        g.add_node(new Coordinates(2, 7));
+        g.add_node(new Coordinates(7, 7));
+        g.add_node(new Coordinates(3, 7));
+        g.add_node(new Coordinates(12, 7));
+        g.add_node(new Coordinates(4, 7));
+        g.add_node(new Coordinates(13, 7));
+        g.add_node(new Coordinates(9, 7));
+        g.add_node(new Coordinates(11, 7));
         assertTrue(g.areBordersConnected());
     }
 }
