4d6cd71800a610f1b00483ac274d4022b8f5b224
KEERTHANA CHANDRASEKAR <cj.keerthana96@gmail.com>
2021-08-13 15:00:08 +0200

    Refactor Board Class - change signature of setPiece method to incorporate Coordinates instead of primitive types. Delete methods getPosFill, getPosColor as it uses data from another class (Feature Envy)

--- src/main/java/Game/Board.java
+++ src/main/java/Game/Board.java
@@ -1,7 +1,6 @@
 package Game;
 
 import java.util.List;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -13,27 +12,23 @@ public class Board {
         IntStream.range(0,size).forEach(i->IntStream.range(0,size).forEach(j->this.grid[i][j] = new Position(i,j)));
     }
     //setters
-    protected void setPiece(int i, int j, Piece_Color color) { this.grid[i][j].setPieceColor(color);}
+    protected void setPiece(Coordinates coordinates, Piece_Color color) { this.grid[coordinates.getRow_idx()][coordinates.getCol_idx()].setPieceColor(color);}
 
     //getters
     public int getSize() { return size; }
 
-    public Position getPos(int i, int j) { return this.grid[i][j];}
+    public Position getPos(Coordinates coordinates) { return this.grid[coordinates.getRow_idx()][coordinates.getCol_idx()];}
 
-    public Pos_Color getPosColor(int i, int j) { return this.grid[i][j].getPosColor();}
-
-    public Piece_Color getPosFill(int i, int j) {return this.grid[i][j].getPieceColor();}
-
-    public Boolean isValidPos(int i, int j){
+    public Boolean isValidPos(Coordinates coordinates){
         List<Integer> range = IntStream.range(0,size).boxed().collect(Collectors.toList());
-        if(range.contains(i) && range.contains(j)) { return Boolean.TRUE; }
+        if(range.contains(coordinates.getRow_idx()) && range.contains(coordinates.getCol_idx())) { return Boolean.TRUE; }
         return Boolean.FALSE;
     }
 
     public void printBoard(){
         for(int i = size-1; i >=0; i--){
             for(int j = 0; j < size; j++){
-                Piece_Color color = getPosFill(i,j);
+                Piece_Color color = getPos(new Coordinates(i,j)).getPieceColor();
                 System.out.print(color.getString() + "\t");
             }
             System.out.println(" ");
