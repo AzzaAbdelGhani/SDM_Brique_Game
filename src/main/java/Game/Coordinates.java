707e1dd85347154ad0b824940f1e04d7c68fab54
KEERTHANA CHANDRASEKAR <cj.keerthana96@gmail.com>
2021-08-13 15:40:01 +0200

    Refactor Coordinate Class - Implemented methods getUp(), getDown(), getRight(), getLeft(), getDiagUp(), getDiagDown() to get the neighbours of a Coordinate

--- src/main/java/Game/Coordinates.java
+++ src/main/java/Game/Coordinates.java
@@ -9,11 +9,19 @@ public class Coordinates {
         this.col_idx = col_idx;
     }
     //getters
-    public int getRow_idx() {
-        return this.row_idx;
-    }
+    public int getRow_idx() { return this.row_idx; }
 
-    public int getCol_idx() {
-        return this.col_idx;
-    }
+    public int getCol_idx() { return this.col_idx; }
+
+    public Coordinates getUp() { return new Coordinates(this.row_idx, this.col_idx+1); }
+
+    public Coordinates getDown() { return new Coordinates(this.row_idx, this.col_idx-1); }
+
+    public Coordinates getLeft() { return new Coordinates(this.row_idx-1, this.col_idx); }
+
+    public Coordinates getRight() { return new Coordinates(this.row_idx+1, this.col_idx); }
+
+    public Coordinates getDiagUp() { return new Coordinates(this.row_idx+1, this.col_idx+1); }
+
+    public Coordinates getDiagDown() { return new Coordinates(this.row_idx-1, this.col_idx-1); }
 }
