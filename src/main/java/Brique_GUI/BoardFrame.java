0c53ddf2ec5ff300772c160ad37ed990d54d14e4
AzzaAbdelGhani <azza.mazher90@gmail.com>
2021-08-15 10:45:44 +0200

    modify the WelcomeFrame by designing the labels and buttons

--- src/main/java/Brique_GUI/BoardFrame.java
+++ src/main/java/Brique_GUI/BoardFrame.java
@@ -1,6 +1,7 @@
 package Brique_GUI;
 
 import Game.Piece_Color;
+import Game.Player;
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,7 +13,7 @@ public class BoardFrame extends JFrame implements MouseListener {
     JFrame board;
     ArrayList<PositionPanel> grid = new ArrayList<>();
 
-    public BoardFrame()
+    public BoardFrame(JFrame frame, Player p1, Player p2)
     {
         this.board= new JFrame("Board");
         this.board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- src/main/java/Brique_GUI/WelcomeFrame.java
+++ src/main/java/Brique_GUI/WelcomeFrame.java
@@ -1,36 +1,89 @@
 package Brique_GUI;
-
+import Game.*;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 public class WelcomeFrame extends JFrame implements ActionListener {
 
-    private  JButton startGame;
-    private JFrame main = new JFrame("Brique Game");
+    private JButton startGame = new JButton("Start Game");;
+    private JButton changeSettings = new JButton("Change Settings");;
+    private JLabel welcomeMessage = new JLabel("Welcome To Brique Game :) ");
+    private JLabel defaultSettingsMessage = new JLabel("Default Settings : P1 (BLACK) vs P2 (WHITE)");
+    private Player P1 = new Player("P1", Piece_Color.BLACK);
+    private Player P2 = new Player("P2", Piece_Color.WHITE);
 
-    public WelcomeFrame()
+    public WelcomeFrame() throws IOException {
+
+        setTitle("Brique");
+        setResizable(false);
+        setLayout(new BorderLayout());
+        URL url = getClass().getResource("");
+        System.out.println(url.toString());
+        JLabel background = new JLabel(new ImageIcon(ImageIO.read(new File("bg.jpg"))));
+        background.setLayout(new FlowLayout(FlowLayout.CENTER, 1,180));
+        add(background,BorderLayout.CENTER);
+        JPanel basePanel = new JPanel();
+        basePanel.setOpaque(false);
+        basePanel.setLayout(new GridLayout(5, 1, 5, 15));
+        designButtonsAndLabels();
+        basePanel.add(welcomeMessage);
+        basePanel.add(defaultSettingsMessage);
+        basePanel.add(changeSettings);
+        basePanel.add(startGame);
+        background.add(basePanel);
+        pack();
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+
+    private void designButtonsAndLabels()
     {
-        this.main.setSize(600,600);
-        this.main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.startGame = new JButton("Start Game");
-        this.main.add(this.startGame, BorderLayout.CENTER);
-        this.main.setVisible(true);
-
+        startGame.setFont(new Font("Arial", Font.PLAIN, 20));
+        startGame.setForeground(Color.GREEN);
+        startGame.setBorderPainted(false);
+        startGame.setFocusPainted(false);
+        startGame.setContentAreaFilled(false);
+        changeSettings.setFont(new Font("Arial", Font.PLAIN, 20));
+        changeSettings.setForeground(Color.CYAN);
+        changeSettings.setBorderPainted(false);
+        changeSettings.setFocusPainted(false);
+        changeSettings.setContentAreaFilled(false);
+        welcomeMessage.setFont(new Font("Serif", Font.ITALIC, 24));
+        welcomeMessage.setForeground(Color.WHITE);
+        defaultSettingsMessage.setFont(new Font("Serif", Font.ITALIC, 15));
+        defaultSettingsMessage.setForeground(Color.lightGray);
+        startGame.addActionListener(this);
+        changeSettings.addActionListener(this);
     }
 
     public void actionPerformed(ActionEvent e)
     {
         if (e.getSource() == startGame)
         {
-            this.main.setVisible(false);
-            new BoardFrame();
+            this.setVisible(false);
+            new BoardFrame(this,P1,P2);
         }
     }
 
     public static void main (String args[])
     {
-        WelcomeFrame w = new WelcomeFrame();
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    WelcomeFrame g = new WelcomeFrame();
+                    g.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
 }
