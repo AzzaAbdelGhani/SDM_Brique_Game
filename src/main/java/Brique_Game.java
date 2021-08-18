df155f3b2ff8aa58261adcf6a0dc9c6ac48372d3
KEERTHANA CHANDRASEKAR <cj.keerthana96@gmail.com>
2021-08-18 16:20:48 +0200

    Refactor Brique_Game - Included GUI feature and takes from user for the option of GUI or CLI

--- src/main/java/Brique_Game.java
+++ src/main/java/Brique_Game.java
@@ -1,7 +1,36 @@
 import Brique_CLI.LaunchGame;
+import Brique_GUI.WelcomeFrame;
+
+import java.awt.*;
+import java.util.Scanner;
 
 public class Brique_Game {
     public static void main(String[] args) {
-        LaunchGame.Play();
+        System.out.println("WELCOME TO BRIQUE GAME : ");
+        System.out.println("Would you like to play in CLI or GUI ?");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        input = input.toLowerCase();
+        while (!input.equals("cli") && !input.equals("gui")) {
+            System.out.println("Please enter cli or gui : ");
+            input = scanner.next();
+        }
+        if (input.equals("cli"))
+        {
+            LaunchGame.Play();
+        }
+        else
+        {
+            EventQueue.invokeLater(() -> {
+                try {
+                    WelcomeFrame g = new WelcomeFrame();
+                    g.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            });
+        }
     }
 }
+
