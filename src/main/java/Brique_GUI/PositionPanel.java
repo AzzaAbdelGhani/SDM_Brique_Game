eed2b377651c83a5dc996ff48743c8fc7b49f128
KEERTHANA CHANDRASEKAR <cj.keerthana96@gmail.com>
2021-08-18 16:10:50 +0200

    Refactor PositionPanel - add methods getRow(), getCol(), getPieceColor(), getPositionColor(); in setPieceColor() added repaint() to color the boxes on clicking

--- src/main/java/Brique_GUI/PositionPanel.java
+++ src/main/java/Brique_GUI/PositionPanel.java
@@ -9,40 +9,59 @@ public class PositionPanel extends JPanel {
     private int row;
     private int col;
     private Piece_Color pColor = Piece_Color.BLANK;
+    private Pos_Color pos_color = Pos_Color.LIGHT;
+    private int panelResolution = 48;
 
-    public PositionPanel(int row, int col)
-    {
+    public PositionPanel(int row, int col) {
         this.row = row;
         this.col = col;
         pos = new JPanel();
-        pos.setSize(new Dimension(48,48));
+        pos.setSize(new Dimension(48, 48));
         pos.setVisible(true);
     }
 
-    public void drawPiece(int i, int j, Graphics g, Color pieceColor)
-    {
-        g.setColor(pieceColor);
-        g.fillOval(i,j,32,32);
-        g.setColor(Color.BLACK);
-        g.drawOval(i,j,32,32);
-    }
-
     @Override
-    protected void paintComponent(Graphics g)
-    {
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor((row + col) % 2 == 0 ? new Color(204,204,204) : new Color(153,153,153));
-        g.fillRect(0,0,48,48);
-        g.drawRect(0,0,48,48);
-        if (pColor == Piece_Color.BLACK)
-        {
-            drawPiece(32,32,g,Color.BLACK);
+        g.setColor((row + col) % 2 == 0 ? new java.awt.Color(204, 204, 204) : new java.awt.Color(153, 153, 153));
+        g.fillRect(0, 0, panelResolution, panelResolution);
+        g.drawRect(0, 0, panelResolution, panelResolution);
+        if ((row + col) % 2 != 0) {
+            this.pos_color = Pos_Color.DARK;
         }
-        else if (pColor == Piece_Color.WHITE)
-        {
-            drawPiece(32,32,g,Color.WHITE);
+        if (pColor == Piece_Color.BLACK) {
+            drawPiece(panelResolution / 4, panelResolution / 4, g, Color.BLACK);
+        } else if (pColor == Piece_Color.WHITE) {
+            drawPiece(panelResolution / 4, panelResolution / 4, g, Color.WHITE);
         }
+
     }
 
-    public void setPiece(Piece_Color pColor) { this.pColor = pColor; }
+    public void drawPiece(int i, int j, Graphics g, Color pieceColor) {
+        g.setColor(pieceColor);
+        g.fillOval(i, j, panelResolution / 2, panelResolution / 2);
+        g.setColor(Color.BLACK);
+        g.drawOval(i, j, panelResolution / 2, panelResolution / 2);
+    }
+
+    public void setPieceColor(Piece_Color pColor) {
+        this.pColor = pColor;
+        repaint();
+    }
+
+    public Piece_Color getPieceColor() {
+        return this.pColor;
+    }
+
+    public Pos_Color getPositionColor() {
+        return this.pos_color;
+    }
+
+    public int getRow() {
+        return this.row;
+    }
+
+    public int getCol() {
+        return this.col;
+    }
 }
