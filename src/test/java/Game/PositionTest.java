cc27dc49411c80f93d0a0ef9a4ccf32c4310893b
KEERTHANA CHANDRASEKAR <cj.keerthana96@gmail.com>
2021-08-23 18:22:36 +0200

    Refactor PositionTest - Provide more test cases with different values.

--- src/test/java/Game/PositionTest.java
+++ src/test/java/Game/PositionTest.java
@@ -1,27 +1,44 @@
 package Game;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PositionTest {
     private final Position pos = new Position(0 ,1);
+
     @Test
     public void checkRowAndColIdx()
     {
+        Position pos = new Position(0, 1);
         assertEquals(pos.getRow_idx(),0);
         assertEquals(pos.getCol_idx(), 1);
     }
-    @Test
-    public void checkPosColor()
+
+    @ParameterizedTest
+    @CsvSource({"1, 4","7, 8", "10, 3", "3, 8", "9, 2"})
+    public void checkDarkPosColor(int row_id, int col_id)
     {
+        Position pos = new Position(row_id, col_id);
         assertEquals(pos.getPosColor(), Pos_Color.DARK);
-        assertEquals(new Position(2, 2).getPosColor(), Pos_Color.LIGHT);
     }
-    @Test
-    public void checkPieceColor()
+
+    @ParameterizedTest
+    @CsvSource({"8, 4","10, 2", "1, 5", "3, 9", "7, 5"})
+    public void checkLightPosColor(int row_id, int col_id)
+    {
+        Position pos = new Position(row_id, col_id);
+        assertEquals(pos.getPosColor(), Pos_Color.LIGHT);
+    }
+
+    /*@ParameterizedTest
+    @CsvSource({"Bln, Bln", "B, B", "W, W"})
+    public void checkPieceColor(String piece, String expected_piece)
     {
         assertEquals(pos.getPieceColor(), Piece_Color.BLANK);
-        pos.setPieceColor(Piece_Color.BLACK);
-        assertEquals(pos.getPieceColor(), Piece_Color.BLACK);
-    }
+        pos.setPieceColor(Piece_Color.valueOf(piece));
+        assertEquals(pos.getPieceColor().getString(), expected_piece );
+    }*/
 }
