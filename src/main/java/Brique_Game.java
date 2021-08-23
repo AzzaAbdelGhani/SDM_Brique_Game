fa3e0cbbc11e9368cb2ac7534a5f54cd331a6dba
KEERTHANA CHANDRASEKAR <cj.keerthana96@gmail.com>
2021-08-23 14:15:55 +0200

    Refactor Brique_Game - Change .equals() method to .equalsIgnoreCase(), including exception from reading text file for CLI game.

--- src/main/java/Brique_Game.java
+++ src/main/java/Brique_Game.java
@@ -2,23 +2,20 @@ import Brique_CLI.LaunchGame;
 import Brique_GUI.WelcomeFrame;
 
 import java.awt.*;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Brique_Game {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("WELCOME TO BRIQUE GAME : ");
         System.out.println("Would you like to play in CLI or GUI ?");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.next();
-        input = input.toLowerCase();
-        while (!input.equals("cli") && !input.equals("gui")) {
+        while (!input.equalsIgnoreCase("cli") && !input.equalsIgnoreCase("gui")) {
             System.out.println("Please enter cli or gui : ");
             input = scanner.next();
         }
-        if (input.equals("cli"))
-        {
-            LaunchGame.Play();
-        }
+        if (input.equals("cli")) { LaunchGame.Play(); }
         else
         {
             EventQueue.invokeLater(() -> {
